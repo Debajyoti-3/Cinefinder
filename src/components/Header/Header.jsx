e51e57601d147c62adda7f7f4316f7762fede46e
Debajyoti Mitra <debajyotimitra369@gmail.com>
2025-06-02 00:51:29 +0530

    fix: fixing import of the api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Logo from "../Logo";
 import { Link, NavLink } from "react-router";
-import MovieSearch from "../API/MovieSearch";
-import { useState } from "react";
+import { SearchContext } from "../../context/SearchContext";
 
 function Header() {
-  const [query, setQuery] = useState("");
+  const { query, setQuery } = useContext(SearchContext);
+  
   return (
     <header>
       <div className="flex justify-center">
@@ -19,23 +19,21 @@ function Header() {
             <div className="flex justify-center border-b-black bg-[#EFEEEA] rounded-[3px] pr-0.5 w-[215px]">
               <NavLink to='/Results'>
                 <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="28"
-                viewBox="0 0 24 24"
-                //onClick={console.log(`Icon clicked`)}
-                //onClick={<MovieSearch/>}
-                
-                className="bg-gray-300 cursor-pointer w-[35px]"
-              >
-                <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
-              </svg>
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="28"
+                  viewBox="0 0 24 24"
+                  className="bg-gray-300 cursor-pointer w-[35px]"
+                >
+                  <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+                </svg>
               </NavLink>
               <input
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 className="border-black p-0.5 w-[180px]"
+                placeholder="Search movies..."
               />
             </div>
             <NavLink to="/About">
